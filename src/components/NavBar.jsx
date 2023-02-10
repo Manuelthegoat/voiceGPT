f5ef7b35010aca5037f71563c21e5366e2a28ae2
Tim <chibuiketimokonkwo@gmail.com>
2023-02-10 13:11:42 +0100

    updating the UI

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,37 @@
-import { HiMenuAlt2, HiPlus } from "react-icons/hi";
+import { HiMenuAlt2, HiPlus, HiChatAlt, HiX} from "react-icons/hi";
 
 function NavBar() {
 	return (
-		<nav>
-			<div className="nav__container">
-				<HiMenuAlt2 className="icon"/>
-				  New chat
-				<HiPlus className="icon"/>
+		<>
+			<header>
+				<nav>
+					<div className="nav__container">
+						<HiMenuAlt2 className="icon" />
+						New chat
+						<HiPlus className="icon" />
+					</div>
+				</nav>
+			</header>
+
+			<div className="nav__menu__wrapper">
+				<div className="nav__menu">
+					<div className="new__chat">
+						<HiPlus /> New chat
+					</div>
+          <div className="chat__history">
+							<div className="history">
+								<HiChatAlt /> What is your name?
+							</div>
+
+              <div className="history">
+								<HiChatAlt /> Write a code?
+							</div>
+						</div>
+				</div>
+
+        {/* <HiX/> */}
 			</div>
-		</nav>
+		</>
 	);
 }
 
