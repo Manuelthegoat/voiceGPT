301e315f76f9d841dfbec1b0844b168067fd6d0e
Tim <chibuiketimokonkwo@gmail.com>
2023-02-10 16:06:45 +0100

    added the message screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import NavBar from "./components/NavBar"
-import ChatBar from './components/ChatBar';
 import './App.css'
-
+import NavBar from "./components/NavBar"
+import ChatBar from './components/ChatBar'
+import SentMessage from './components/SentMessage';
+import AIAnswer from './components/AIAnswer';
 
 function App() {
   
   return <>
     <NavBar/>
+    <SentMessage/>
+    <AIAnswer />
     <ChatBar/>
   </>
 }
--- src/components/ChatBar.jsx
+++ src/components/ChatBar.jsx
@@ -2,12 +2,17 @@
  * Implement auto resize chat area later
 */
 
+import TextareaAutosize from 'react-textarea-autosize';
+import {FiSend} from "react-icons/fi"
+
 const ChatBar = () => {
 
-    return <div className="chat__wrapper">
+    return <div className="chatbox__wrapper">
         <div className="chatbox">
-            <input type="text" />
+            <TextareaAutosize/>
+            <FiSend className="send__icon"/>
         </div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     </div>
 }
 
