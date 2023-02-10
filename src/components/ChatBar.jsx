0c50ad15633bba7f76a624842b73b8f801fa6762
Tim <chibuiketimokonkwo@gmail.com>
2023-02-10 16:25:32 +0100

    fixed textarea issue:

--- src/components/ChatBar.jsx
+++ src/components/ChatBar.jsx
@@ -9,7 +9,7 @@ const ChatBar = () => {
 
     return <div className="chatbox__wrapper">
         <div className="chatbox">
-            <TextareaAutosize/>
+            <TextareaAutosize maxRows={6}/>
             <FiSend className="send__icon"/>
         </div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. 
