7952cf6f094d5653348dbda3009ac1470d4f663b
Tim <chibuiketimokonkwo@gmail.com>
2023-04-25 21:07:25 +0100

    coming soon

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Soon from "./Soon"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Soon />
   </React.StrictMode>,
 )
